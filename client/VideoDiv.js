6a259dfd54a3bacc0690b5eb4aa4fd8ec6dc84b9
Anwesh43 <anweshthecool0@gmail.com>
2017-05-07 20:08:38 +0530

    adding fixes for video div

--- client/VideoDiv.js
+++ client/VideoDiv.js
@@ -11,17 +11,19 @@ class VideoDiv {
         const w = window.innerWidth ,h = window.innerHeight
         video.width = w
         video.height = h
+        canvas.width = w
+        canvas.height = h
         video.src = this.src
+        const img = document.createElement('img')
+        document.body.appendChild(img)
         setInterval(()=>{
           context.drawImage(video,0,0,w,h)
           context.font = context.font.replace(/\d+/,w/20)
           context.fillStyle='white'
           context.fillText(this.title,w/2-context.measureText(this.title).width/2,3*h/5)
           context.font = context.font.replace(/\d+/,w/40)
-          context.fillText(this.title,w/2-context.measureText(this.title).width/2,4*h/5)
-          const img = document.createElement('img')
+          context.fillText(this.subtitle,w/2-context.measureText(this.subtitle).width/2,4*h/5)
           img.src = canvas.toDataURL()
-          document.body.appendChild(img)
-        },100)
+        },1)
     }
 }
